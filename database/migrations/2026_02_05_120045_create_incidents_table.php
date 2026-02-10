26fb3c2583999a4dcaead0f22baad4180738e843
lorenzo <lorenzoschemmekes@gmail.com>
2026-02-10 13:03:42 +0100

    commit2

--- database/migrations/2026_02_05_120045_create_incidents_table.php
+++ database/migrations/2026_02_05_120045_create_incidents_table.php
@@ -9,20 +9,15 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('incidents', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
-
-            $table->string('title');
-            $table->text('description');
-            $table->string('location');
-            $table->string('type');
-            $table->dateTime('incident_at');
-
-            $table->string('status')->default('ingediend');
-            $table->string('attachment')->nullable();
-
-            $table->timestamps();
-        });
+    $table->id();
+    $table->string('title');
+    $table->text('description');
+    $table->string('location');
+    $table->string('type');
+    $table->dateTime('incident_at');
+    $table->string('attachment')->nullable();
+    $table->timestamps();
+});
     }
 
     public function down(): void
