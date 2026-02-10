237515e9d302a99f1a2f245576582a06c66635e4
lorenzo <lorenzoschemmekes@gmail.com>
2026-02-10 12:53:36 +0100

    1

--- routes/web.php
+++ routes/web.php
@@ -3,22 +3,20 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\IncidentController;
 
+
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
 
-// Minimal named login route to satisfy `auth` middleware redirects.
-Route::get('/login', function () {
-    return redirect('/');
-})->name('login');
 
-// Redirect legacy or mistyped /incidents/index to the canonical /incidents route.
+Route::get('/incidents', [IncidentController::class, 'index'])
+    ->name('incidents.index');
+
+Route::get('/incidents/create', [IncidentController::class, 'create'])
+    ->name('incidents.create');
+
+Route::post('/incidents', [IncidentController::class, 'store'])
+    ->name('incidents.store');
+
+
 Route::redirect('/incidents/index', '/incidents');
-
-
-
-Route::middleware('auth')->group(function () {
-    Route::get('/incidents', [IncidentController::class, 'index'])->name('incidents.index');
-    Route::get('/incidents/create', [IncidentController::class, 'create'])->name('incidents.create');
-    Route::post('/incidents', [IncidentController::class, 'store'])->name('incidents.store');
-}); 
